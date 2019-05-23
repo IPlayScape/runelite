aba5e1d1aa28042156c2d68c8994c27037465265
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-23 14:21:58 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/tickcounter/TickCounterPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/tickcounter/TickCounterPlugin.java
@@ -29,7 +29,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.PluginType;
 
-@PluginDescriptor(name = "Tick Counter",
+@PluginDescriptor(name = "✓ Tick Counter",
 	description = "Counts combat activity for nearby players",
 	enabledByDefault = false,
 	type = PluginType.PVP
