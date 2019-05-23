e5d46c2695c64192f27da191ed45fa6bd3c2a11e
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-23 14:21:58 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/flinching/FlinchingPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/flinching/FlinchingPlugin.java
@@ -50,7 +50,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Flinching Timer",
+	name = "✓ Flinching Timer",
 	description = "Time your attacks while flinching",
 	tags = {"overlay", "flinching", "timers", "combat"},
 	type = PluginType.PVM,
