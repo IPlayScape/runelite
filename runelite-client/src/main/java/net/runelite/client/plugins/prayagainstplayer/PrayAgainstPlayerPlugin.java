676d5d27504313f41b50267bb4af7720da6a87df
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-23 14:21:58 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/prayagainstplayer/PrayAgainstPlayerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayagainstplayer/PrayAgainstPlayerPlugin.java
@@ -53,7 +53,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.ImageUtil;
 
 @PluginDescriptor(
-	name = "Pray Against Player",
+	name = "✓ Pray Against Player",
 	description = "Use plugin in PvP situations for best results!!",
 	tags = {"highlight", "pvp", "overlay", "players"},
 	type = PluginType.PVP,
