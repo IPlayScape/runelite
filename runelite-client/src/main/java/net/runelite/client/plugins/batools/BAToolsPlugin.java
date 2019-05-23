b6734260a12e05825607507ff8a40c24eba0697a
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-23 14:21:58 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/batools/BAToolsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/batools/BAToolsPlugin.java
@@ -96,7 +96,7 @@ import net.runelite.client.menus.MenuManager;
 
 @Slf4j
 @PluginDescriptor(
-	name = "BA Tools",
+	name = "✕ BA Tools",
 	description = "Custom tools for Barbarian Assault",
 	tags = {"minigame", "overlay", "timer"},
 	type = PluginType.PVM,
