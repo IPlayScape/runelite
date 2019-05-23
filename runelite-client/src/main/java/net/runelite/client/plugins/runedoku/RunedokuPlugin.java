516634d997f7b6962fcccfea4029031662a6b770
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-23 14:21:58 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/runedoku/RunedokuPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/runedoku/RunedokuPlugin.java
@@ -36,7 +36,7 @@ import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 @PluginDescriptor(
-	name = "Runedoku Solver",
+	name = "✓ Runedoku Solver",
 	description = "Show solutions for current Runedoku puzzle.",
 	tags = {"overlay", "runedoku", "sudoku", "puzzle", "solving"},
 	type = PluginType.UTILITY,
