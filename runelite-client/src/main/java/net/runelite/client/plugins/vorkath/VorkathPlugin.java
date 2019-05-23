89a0a87e40137cc229d4ca7c46d4ad9fbb84f752
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-23 14:21:58 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/vorkath/VorkathPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/vorkath/VorkathPlugin.java
@@ -46,7 +46,7 @@ import net.runelite.client.util.ImageUtil;
 import org.apache.commons.lang3.ArrayUtils;
 
 @PluginDescriptor(
-	name = "Vorkath Helper",
+	name = "✓ Vorkath Helper",
 	description = "Count vorkath attacks, and which phase is coming next",
 	tags = {"combat", "overlay", "pve", "pvm"},
 	type = PluginType.PVM,
