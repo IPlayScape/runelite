375b5b33aef19e2063c7080bd760279d0a640136
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-30 17:21:23 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/entertochat/EnterToChatPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/entertochat/EnterToChatPlugin.java
@@ -50,7 +50,7 @@ import net.runelite.client.ui.JagexColors;
 import net.runelite.client.util.ColorUtil;
 
 @PluginDescriptor(
-	name = "✓ Press Enter to Chat",
+	name = "Press Enter to Chat",
 	description = "'Press Enter to Chat'",
 	tags = {"enter", "chat"},
 	type = PluginType.UTILITY,
--- runelite-client/src/main/java/net/runelite/client/plugins/prayeralert/PrayerAlertPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayeralert/PrayerAlertPlugin.java
@@ -25,7 +25,7 @@ import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 @PluginDescriptor(
-	name = "✓ Prayer Alerter",
+	name = "Prayer Alerter",
 	description = "Alert the player when prayer is low",
 	tags = {"prayer", "overlay"},
 	type = PluginType.UTILITY,
