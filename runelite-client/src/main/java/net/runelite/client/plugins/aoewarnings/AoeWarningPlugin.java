b6ad7d6bcadfe8f984fd82fb65645b306ad64bb0
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-30 17:21:23 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/aoewarnings/AoeWarningPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/aoewarnings/AoeWarningPlugin.java
@@ -60,7 +60,7 @@ import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 @PluginDescriptor(
-	name = "✓ AoE Warnings",
+	name = "AoE Warnings",
 	description = "Shows the final destination for AoE Attack projectiles",
 	tags = {"bosses", "combat", "pve", "overlay"},
 	type = PluginType.PVM
