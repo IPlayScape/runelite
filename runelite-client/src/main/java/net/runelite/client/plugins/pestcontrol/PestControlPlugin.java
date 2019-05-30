e0170b7fa266db8abc80e100e0fafe381b1bb41d
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-30 17:21:23 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/pestcontrol/PestControlPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pestcontrol/PestControlPlugin.java
@@ -67,7 +67,7 @@ import net.runelite.client.util.Text;
 
 @Slf4j
 @PluginDescriptor(
-	name = "✓ Pest Control",
+	name = "Pest Control",
 	description = "Show helpful information for the Pest Control minigame",
 	tags = {"minigame", "overlay"}
 )
