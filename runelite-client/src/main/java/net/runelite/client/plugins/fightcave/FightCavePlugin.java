6448c0ffef28f2c051e5283a4340e72ee57ef53c
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-30 17:21:23 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/fightcave/FightCavePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/fightcave/FightCavePlugin.java
@@ -57,7 +57,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 import org.apache.commons.lang3.ArrayUtils;
 
 @PluginDescriptor(
-	name = "✓ Fight Cave",
+	name = "Fight Cave",
 	description = "Displays current and upcoming wave monsters in the Fight Caves",
 	tags = {"bosses", "combat", "minigame", "overlay", "pve", "pvm", "jad", "fire", "cape", "wave"},
 	type = PluginType.PVM,
