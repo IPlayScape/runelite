0e077d80ca59b414230320964430df1e1effc71c
Zyxts <39000646+Zyxts@users.noreply.github.com>
2019-05-30 17:21:23 +0100

    :)

--- runelite-client/src/main/java/net/runelite/client/plugins/pvptools/PvpToolsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pvptools/PvpToolsPlugin.java
@@ -63,7 +63,7 @@ import net.runelite.client.util.Text;
 import org.apache.commons.lang3.ArrayUtils;
 
 @PluginDescriptor(
-	name = "✕ PvP Tools",
+	name = "PvP Tools",
 	description = "Enable the PvP Tools panel",
 	tags = {"panel", "pvp", "pk", "pklite", "renderself"},
 	type = PluginType.PVP,
